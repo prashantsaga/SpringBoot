70644d72a03046fd867943ea806cc8fbfffa3681
prashantsaga <sagar140793@gmail.com>
2023-02-28 12:30:18 +0530

    check

--- class-test/class-test/src/test/java/com/example/classtest/ClassTestApplicationTests.java
+++ class-test/class-test/src/test/java/com/example/classtest/ClassTestApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.classtest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBoot 
+@SpringBoot Test
 class ClassTestApplicationTests {
 
 	@Test
